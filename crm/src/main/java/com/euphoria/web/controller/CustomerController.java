43d93c879fde35c0ec1667450862718eebf9aee2
dev-lmc <walawe@gmail.com>
2014-10-25 11:10:58 +1100

    delete customer

--- crm/src/main/java/com/euphoria/web/controller/CustomerController.java
+++ crm/src/main/java/com/euphoria/web/controller/CustomerController.java
@@ -18,60 +18,80 @@ import com.euphoria.data.repository.CustomerSearchRepository;
 
 @Controller
 public class CustomerController {
-	private static final Logger log = Logger.getLogger(CustomerController.class);
-	
-	private static final String CUSTOMER = "customer";
-	private static final String CUSTOMER_VIEW = CUSTOMER;
-	private static final String CUSTOMERS = "customers";
-	private static final String DEFAULT_VIEW = CUSTOMERS;
-	private static final String PATH = "/" + CUSTOMER;
+    private static final Logger log = Logger.getLogger(CustomerController.class);
 
-	@Autowired
-	private CustomerRepository customerRepository;
-	
-	@Autowired
-	private CustomerSearchRepository customerSearchRepository;
-	
-	@RequestMapping(value = PATH, method = RequestMethod.GET)
-	public String customerHome(ModelMap model) {
-		model.addAttribute(CUSTOMER, new Customer());
-		return DEFAULT_VIEW;
-	}
+    private static final String CUSTOMER = "customer";
+    private static final String CUSTOMERS = "customers";
+    private static final String DEFAULT_VIEW = CUSTOMERS;
+    private static final String CUSTOMER_VIEW = CUSTOMER;
+    private static final String PATH = "/" + CUSTOMER;
+    private static final String DELETE_CUSTOMER_VIEW = "/" + CUSTOMER + "/delete";
+    private static final String PAGE_MESSAGE = "message";
 
-	@RequestMapping(value = PATH, method = RequestMethod.POST,params="keyword")
-	public String customerSearch(@RequestParam String keyword, ModelMap model) {
-		List<Customer> result = null;
-		if (keyword != null) {
-			result = customerRepository.findDistinctCustomerByKeyword(keyword);
-		}
-		model.addAttribute(CUSTOMERS, result);
-		model.addAttribute(CUSTOMER, new Customer());
-		model.addAttribute("keyword", keyword);
-		return DEFAULT_VIEW;
-	}
+    @Autowired
+    private CustomerRepository customerRepository;
 
-	@RequestMapping(value = PATH, method = RequestMethod.POST)
-	public String addCustomer(@ModelAttribute Customer customer,
-			ModelMap model) {
-		if (customer != null) {
-			Customer result = customerRepository.save(customer);
-			model.addAttribute(CUSTOMER, result);
-			return CUSTOMER_VIEW;
-		}
-		return DEFAULT_VIEW;
-	}
-	
-	@RequestMapping(value = PATH + "/{id}", method = RequestMethod.GET)
-	public String customerId(@PathVariable Long id, ModelMap model) {
-		Customer customer = customerRepository.findOne(id);
+    @Autowired
+    private CustomerSearchRepository customerSearchRepository;
 
-		String view;
-		if (customer != null) {
-			model.addAttribute(CUSTOMER, customer);
-			view = CUSTOMER_VIEW;
-		} else {
-			view = DEFAULT_VIEW;
-		}
-		return view;
-	}
+    @RequestMapping(value = PATH, method = RequestMethod.GET)
+    public String customerHome(ModelMap model) {
+        model.addAttribute(CUSTOMER, new Customer());
+        return DEFAULT_VIEW;
+    }
+
+    @RequestMapping(value = PATH, method = RequestMethod.POST, params = "keyword")
+    public String customerSearch(@RequestParam String keyword, ModelMap model) {
+        List<Customer> result = null;
+        if (keyword != null) {
+            result = customerRepository.findDistinctCustomerByKeyword(keyword);
+        }
+        model.addAttribute(CUSTOMERS, result);
+        model.addAttribute(CUSTOMER, new Customer());
+        model.addAttribute("keyword", keyword);
+        return DEFAULT_VIEW;
+    }
+
+    @RequestMapping(value = PATH, method = RequestMethod.POST)
+    public String addCustomer(@ModelAttribute Customer customer, ModelMap model) {
+        if (customer != null) {
+            Customer result = customerRepository.save(customer);
+            model.addAttribute(CUSTOMER, result);
+            return CUSTOMER_VIEW;
+        }
+        return DEFAULT_VIEW;
+    }
+
+    @RequestMapping(value = PATH + "/{id}", method = RequestMethod.GET)
+    public String customerId(@PathVariable Long id, ModelMap model) {
+        Customer customer = customerRepository.findOne(id);
+
+        String view;
+        if (customer != null) {
+            model.addAttribute(CUSTOMER, customer);
+            view = CUSTOMER_VIEW;
+        } else {
+            view = DEFAULT_VIEW;
+        }
+        return view;
+    }
+
+    @RequestMapping(value = DELETE_CUSTOMER_VIEW, method = RequestMethod.POST, params = "customerId")
+    public String deleteCustomer(@RequestParam Long customerId, ModelMap model) {
+        Customer customer = customerRepository.findOne(customerId);
+
+        String message;
+        if (customer != null) {
+            //customerRepository.delete(customerId);
+            message = "Customer " + customerId + " has been deleted.";
+        } else {
+            message = "Error occurred while deleting customer.";
+        }
+
+        model.addAttribute(CUSTOMER, new Customer());
+        model.addAttribute(PAGE_MESSAGE, message);
+
+        return DEFAULT_VIEW;
+    }
+
 }
--- crm/src/main/webapp/WEB-INF/views/customers.jsp
+++ crm/src/main/webapp/WEB-INF/views/customers.jsp
@@ -1,13 +1,14 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 
 <tiles:insertDefinition name="defaultTemplate">
     <tiles:putAttribute name="content">
         <div class="content">
             <div class="action">
+                <c:if test="${message != null}">${message}</c:if>
                 <h1>Search for an existing customer</h1>
-                <form method="POST" action="customer">
+                <form:form method="POST" action="customer">
                     <div>
                         <label for="firstName">Keyword</label> <input name="keyword" value="${keyword}"
                             placeholder="Enter first name, last name or email address" type="text" />
@@ -15,7 +16,7 @@
                     <div>
                         <input type="submit" value="Search" id="search" class="button" />
                     </div>
-                </form>
+                </form:form>
                 <c:choose>
                     <c:when test="${customers == null}" />
                     <c:when test="${empty customers}">
@@ -26,12 +27,22 @@
                             <tiles:putAttribute name="theader">
                                 <tr>
                                     <th>Customer name</th>
+                                    <th>Actions</th>
                                 </tr>
                             </tiles:putAttribute>
                             <tiles:putAttribute name="tbody">
                                 <c:forEach var="cust" items="${customers}">
                                     <tr>
-                                        <td><a href="customer/${cust.customerId}"><c:out value="${cust.firstName}" /></a></td>
+                                        <td><c:out value="${cust.firstName}" /></td>
+                                        <td>
+                                            <a href="customer/${cust.customerId}" title="Edit customer" class="floatleft">
+                                                <img border="0" hspace="2" class="link" alt="Edit" height="20px" width="20px" src="<c:url value="/"/>resources/images/icons/edit_user.png" />
+                                            </a>
+                                            <form:form method="POST" action="customer/delete" class="zero-margin">
+                                                <input type="hidden" name="customerId" value="${cust.customerId}" />
+                                                <input type="submit" value="" id="delete" alt="Delete cusomter" class="icon-user-delete" />
+                                            </form:form>
+                                        </td>
                                 </c:forEach>
                             </tiles:putAttribute>
                         </tiles:insertDefinition>
